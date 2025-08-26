06703fe01e1d427e85b99115742e6dff2610c97c
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-08-26 13:18:51 +0400

    Added logic for methods GET, POST and PATCH, and create file errors.py

--- server.py
+++ server.py
@@ -1,29 +1,92 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.views import MethodView
+from sqlalchemy.exc import IntegrityError
+
+from models import Session, Announcement
+from errors import HttpError
 
 
 app = Flask("app")
+
+'''Декоратор, открывает сессию'''
+@app.before_request
+def before_request():
+    session = Session()
+    request.session = session
+
+'''Декоратор, закрывает сессию'''
+@app.after_request
+def after_request(response):
+    request.session.close()
+    return response
+
+'''Декоратор, обработчик ошибок'''
+@app.errorhandler(HttpError)
+def error_handler(err: HttpError):
+    http_response = jsonify({"Ошибка": err.message})
+    http_response.status_code = err.status_code
+    return http_response
+
+'''Функция по получению объявления по id'''
+def get_announcement_by_id(announcement_id: int) -> Announcement:
+    announcement = request.session.get(Announcement, announcement_id)
+    if announcement is None:
+        raise HttpError(status_code=404, message="Объявление не найдено!")
+    return announcement
+
+'''Функция по созданию объявления, добавлению в сессию и коммиту'''
+def add_announcement(announcement: Announcement) -> Announcement:
+    try:
+        request.session.add(announcement)
+        request.session.commit()
+    except IntegrityError:
+        raise HttpError(status_code=409, message="Такое объявление уже существует!")
+
 
 '''Приветствие на главной странице сайта'''
 @app.route("/")
 def cmd_hello():
     return "Добро пожаловать!"
 
-class AnnouncementView(MethodView):
 
-    def get(self, announcement_id):
+class AnnouncementView(MethodView):
+    '''View-класс по обработке методов CRUD'''
+    
+    def get(self, announcement_id: int):
         '''Получение объявлений'''
-        pass
+        announcement = get_announcement_by_id(announcement_id)
+        return jsonify(announcement.json_format)
+    
 
     def post(self):
         '''Создание объявления'''
-        pass
+        json_data = request.json
+        announcement = Announcement(
+            headline=json_data["headline"],
+            description=json_data["description"],
+            owner=json_data["owner"]
+        )
+        add_announcement(announcement)
+        return jsonify(announcement.id_json)
 
-    def patch(self, announcement_id):
+
+    def patch(self, announcement_id: int):
         '''Изменение объявления'''
-        pass
+        json_data = request.json
+        announcement = get_announcement_by_id(announcement_id)
 
-    def delete(self, announcement_id):
+        if "headline" in json_data:
+            announcement.headline = json_data["headline"]
+        if "description" in json_data:
+            announcement.description = json_data["description"]
+        if "owner" in json_data:
+            announcement.owner = json_data["owner"]
+
+        add_announcement(announcement)
+        return jsonify(announcement.id_json)
+
+
+    def delete(self, announcement_id: int):
         '''Удаление объявления'''
         pass
 
