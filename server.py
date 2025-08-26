59c0bc156c9c2b9731a110d30a212256cd38a694
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-08-26 12:28:53 +0400

    Create sample CRUD for class AnnouncementView

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask.views import MethodView
 
 
 app = Flask("app")
@@ -7,6 +8,35 @@ app = Flask("app")
 @app.route("/")
 def cmd_hello():
     return "Добро пожаловать!"
+
+class AnnouncementView(MethodView):
+
+    def get(self, announcement_id):
+        '''Получение объявлений'''
+        pass
+
+    def post(self):
+        '''Создание объявления'''
+        pass
+
+    def patch(self, announcement_id):
+        '''Изменение объявления'''
+        pass
+
+    def delete(self, announcement_id):
+        '''Удаление объявления'''
+        pass
+
+
+announcement_view = AnnouncementView.as_view("announcement_View")
+
+# Регистрация в маршрутизаторе метода POST
+app.add_url_rule("/announcement/", view_func=announcement_view, methods=["POST"])
+
+# Регистрация в маршрутизаторе методов GET, PATCH, DELETE
+app.add_url_rule(
+    "/announcement/<int:announcement_id>/",
+    view_func=announcement_view, methods=["GET", "PATCH", "DELETE"])
 
 
 if __name__ == "__main__":
